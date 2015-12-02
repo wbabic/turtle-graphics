bf1a00cde3f7b5eb817c6a0d5d95a29dd8f6cbb0
Walter P Babic <wbabic@mac.com>
2015-12-01 21:30:00 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,46 @@
+(defproject turtle-graphics "0.1.0-SNAPSHOT"
+  :description "Turtle Graphics for clojurescript"
+  :url "http://wbabic.github.io"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [devcards "0.2.1"]
+                 [sablono "0.4.0"]
+                 #_[org.omcljs/om "0.9.0"]
+                 [reagent "0.5.1"]]
+
+  :plugins [[lein-cljsbuild "1.1.1"]
+            [lein-figwheel "0.5.0-1"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "target"]
+
+  :source-paths ["src"]
+
+  :cljsbuild {
+              :builds [{:id "devcards"
+                        :source-paths ["src"]
+                        :figwheel { :devcards true } ;; <- note this
+                        :compiler { :main       "turtle-graphics.core"
+                                    :asset-path "js/compiled/devcards_out"
+                                    :output-to  "resources/public/js/compiled/turtle_graphics_devcards.js"
+                                    :output-dir "resources/public/js/compiled/devcards_out"
+                                    :source-map-timestamp true }}
+                       {:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main       "turtle-graphics.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/turtle_graphics.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:main       "turtle-graphics.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/turtle_graphics.js"
+                                   :optimizations :advanced}}]}
+
+  :figwheel { :css-dirs ["resources/public/css"] })
